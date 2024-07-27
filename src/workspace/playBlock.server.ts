8b6aab51c0b94e158d51b42ef51dc259f6cb0065
AquaJo <84229101+AquaJo@users.noreply.github.com>
2024-07-27 15:12:22 +0200

    cleaner state of boilerplate

--- src/workspace/playBlock.server.ts
+++ src/workspace/playBlock.server.ts
@@ -3,7 +3,7 @@ print("HEYY FROM WORKSPACE");
 const tweenService = game.GetService("TweenService");
 const tweenInfo = new TweenInfo(1, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut, 0, false, 0);
 
-const testblock = script.Parent!.WaitForChild("part") as Part;
+const testblock = script.Parent!.WaitForChild("block") as Part;
 const currentPosition = testblock.Position;
 const newPosition = currentPosition.add(new Vector3(30, 0, 0)); // Add +30 in the x direction
 
@@ -14,6 +14,6 @@ print("Tween completed");
 
 // now + 50 in z direction with the help of cframes
 const currentCFrame = testblock.CFrame;
-const newCFrame = currentCFrame.mul(new CFrame(0, 0, 50));
+const newCFrame = currentCFrame.mul(new CFrame(0, 0, 200));
 const tween2 = tweenService.Create(testblock, tweenInfo, { CFrame: newCFrame });
 tween2.Play();
