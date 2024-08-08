29cbaad1df7256570ce93b222254aa8a05a54e87
AquaJo <84229101+AquaJo@users.noreply.github.com>
2024-08-08 20:55:43 +0200

    adjusted submodule updating and added docker option while loading docker as submodule

--- scripts/start.js
+++ scripts/start.js
@@ -1,4 +1,5 @@
 const RobloxStudioManager = require("./classes/RobloxStudioManager");
+const args = process.argv.slice(2);
 // Instantiate the class to run everything
 //let instance = new RobloxStudioManager("noPluginLoading");
-let instance = new RobloxStudioManager();
+let instance = new RobloxStudioManager(args);
